f5abd0cae4f06383b86a878ba3731dd706601071
mitrich772 <mitrichdmitrich772@gmail.com>
2024-11-05 22:13:18 +0300

    add func for dynamic Astep

--- main.cpp
+++ main.cpp
@@ -6,7 +6,12 @@
 #include <vector>
 #include <functional>
 #include <stdexcept>
-#define MULT 1
+#define MULT 2
+#define MULT2 20
+#define INIT_ALPHA 0.1 // Снижен начальный шаг
+#define H 1e-8        // Точность для вычисления градиента
+#define TOL 1e-8      // Порог для остановки минимизации
+#define ITERATIONS 100000
 using namespace std;
 
 class MathFunction {
@@ -16,7 +21,7 @@ public:
 
     MathFunction(function<double(const vector<double>&)> f) : func(f){}
 
-    double evaluate(const vector<double>& args) const {
+    double evaluate(const vector<double> args) const {
         return func(args);
     }
 };
@@ -51,7 +56,7 @@ public:
     static MathFunction secondMathFunc;
 
 
-    static Point computeGradient(const MathFunction& func, const Point point, double h = 1e-6) {
+    static Point computeGradient(const MathFunction& func, const Point point, double h = H) {
         vector<double> grad_coords(point.coords.size());
 
         for (unsigned int i = 0; i < point.coords.size(); ++i) {
@@ -61,16 +66,39 @@ public:
         return Point(grad_coords);
     }
 
-    static Point minWithConjugateGradient(const MathFunction func, Point point,double h = 1e-6, int maxIter = 1000) {
+    static double lineSearch(const MathFunction& func, const Point& point, const Point& direction, double tol = 1e-6, double initialAlpha = INIT_ALPHA) {
+        double alpha = initialAlpha;
+        double stepSizeReduction = 0.5; // Коэффициент уменьшения шага
+        double minAlpha = tol; // Минимальный шаг, при котором остановится поиск
+        double bestAlpha = 0.0;
+        double bestValue = func.evaluate(point.coords); // Начальное значение функции
+
+        while (alpha > minAlpha) {
+            Point testPoint = point + (direction * alpha); // Новая точка в направлении direction с шагом alpha
+            double testValue = func.evaluate(testPoint.coords); // Вычисляем значение функции в новой точке
+
+            if (testValue < bestValue) {
+                bestValue = testValue;
+                bestAlpha = alpha; // Обновляем лучший найденный шаг
+            } else {
+                // Уменьшаем шаг, так как текущий не улучшает значение функции
+                alpha *= stepSizeReduction;
+            }
+        }
+
+        return bestAlpha; // Возвращаем лучший найденный шаг
+    }
+
+    static Point minWithConjugateGradient(const MathFunction func, Point point,double h = H, int maxIter = ITERATIONS) {
         Point grad = computeGradient(func, point); // Градиент теперь Point
         Point d = grad;
         double beta, gradNorm;
 
         swapCoords(d); // Начальное направление
 
-        for (int k = 0; k < maxIter; ++k) {
+        for (int k = 0; k < maxIter; k++) {
             // Поиск шага
-            double alpha = 0.0001;  // Пример фиксированного шага
+            double alpha = lineSearch(func, point, d);  // шаг
             Point pointNew = point + (d * alpha);
 
             // Вычисляем новый градиент и проверяем условие остановки
@@ -112,7 +140,7 @@ private:
                (grad.coords[0] * grad.coords[0] + grad.coords[1] * grad.coords[1]);
     }
 
-    static void swapCoords(Point d) {
+    static void swapCoords(Point& d) {
         for (double& v : d.coords) {
             v = -v;
         }
@@ -136,25 +164,64 @@ MathFunction conjugateGradient::secondMathFunc = MathFunction(
         if (args.size() != 2) {
             throw invalid_argument("Function requires exactly 2 arguments.");
         }
-        return (-1*(((x-3)*(x-3)*(y-4)*(y-4)) + 10*((x-5)*(x-5)) + (y-4)*(y-4) + 1));
+        return (-1 / ((x-3)*(x-3)*(y-4)*(y-4) + 10*(x-5)*(x-5) + (y-4)*(y-4) + 1));
         }
     ); // реализация 2й
 
-int main(int argc, char *argv[]) {
-
+int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
-    conjugateGradient::minWithConjugateGradient(conjugateGradient::firstMathFunc,Point{-50,-50});
-    for(int i = 0;i<points.size()-1;i++){
-        w.printStrelka(points[i]*MULT,points[i+1]*MULT,Qt::blue);
-    }
-    cout<<points[points.size()-1].coords[0] << "|" << points[points.size()-1].coords[1]<<endl;
+
+    // Первое минимизирование
+    cout << "Starting minimization of the first function..." << endl;
     points.clear();
-    conjugateGradient::minWithConjugateGradient(conjugateGradient::secondMathFunc,Point{5,50});
-    for(int i = 0;i<points.size()-1;i++){
-        w.printStrelka(points[i]*MULT,points[i+1]*MULT,Qt::red);
+    Point startPoint1{-150, -100};
+
+    // Выполнение минимизации для первой функции
+    Point minPoint1 = conjugateGradient::minWithConjugateGradient(conjugateGradient::firstMathFunc, startPoint1);
+    if (!points.empty()) {
+        cout << "First function minimized." << endl;
+
+        // Вывод результатов и визуализация
+        for(size_t i = 0; i < points.size() - 1; ++i) {
+            w.printStrelka(points[i] * MULT, points[i + 1] * MULT, Qt::blue);
+        }
+
+        // Итоговый результат минимизации первой функции
+        double resultFirst = conjugateGradient::firstMathFunc.evaluate(points.back().coords);
+        cout << "Minimum of the first function at point ("
+             << points.back().coords[0] << ", "
+             << points.back().coords[1] << ") := "
+             << resultFirst << endl;
+    } else {
+        cout << "Error: No points found during minimization of the first function." << endl;
     }
-    cout<<points[points.size()-1].coords[0] << "|" << points[points.size()-1].coords[1]<<endl;
+
+    // Второе минимизирование
+    cout << "\nStarting minimization of the second function..." << endl;
+    points.clear();
+    Point startPoint2{0,0};
+
+    // Выполнение минимизации для второй функции
+    Point minPoint2 = conjugateGradient::minWithConjugateGradient(conjugateGradient::secondMathFunc, startPoint2);
+    if (!points.empty()) {
+        cout << "Second function minimized." << endl;
+        // Вывод результатов и визуализация
+        for (size_t i = 0; i < points.size() - 1; ++i) {
+            w.printStrelka(points[i] * MULT2, points[i + 1] * MULT2, Qt::red);
+        }
+
+        // Итоговый результат минимизации второй функции
+        double resultSecond = conjugateGradient::secondMathFunc.evaluate(points.back().coords);
+        cout << "Minimum of the second function at point ("
+             << points.back().coords[0] << ", "
+             << points.back().coords[1] << ") := "
+             << resultSecond << endl;
+    } else {
+        cout << "Error: No points found during minimization of the second function." << endl;
+    }
+
+    // Показ окна
     w.show();
     return a.exec();
 }
