ddb6c59c3a83aa1c0127d8e4d450201ae489c312
mitrich772 <mitrichdmitrich772@gmail.com>
2024-11-09 20:59:51 +0300

    work version

--- main.cpp
+++ main.cpp
@@ -7,11 +7,12 @@
 #include <functional>
 #include <stdexcept>
 #define MULT 2
-#define MULT2 20
-#define INIT_ALPHA 0.1 // Снижен начальный шаг
-#define H 1e-8        // Точность для вычисления градиента
-#define TOL 1e-8      // Порог для остановки минимизации
-#define ITERATIONS 100000
+#define MULT2 2
+#define INIT_ALPHA 100
+#define H 1e-8
+#define TOL 1e-8
+#define ITERATIONS 10000
+#define N 2
 using namespace std;
 
 class MathFunction {
@@ -23,6 +24,9 @@ public:
 
     double evaluate(const vector<double> args) const {
         return func(args);
+    }
+    double evaluate(const Point args) const {
+        return func(args.coords);
     }
 };
 //struct Point {
@@ -47,15 +51,15 @@ public:
 //        return result;
 //    }
 //};
-static vector<Point> points;
 
+static vector<Point> points;
 class conjugateGradient{ //x0 -> R n-ое (x1,x2,x3)
 public:
-
     static MathFunction firstMathFunc;
     static MathFunction secondMathFunc;
 
-
+    static double minimizeAlpha(const MathFunction& f, const Point& start, const Point& direction, double a, double b, double c, double tol);
+    static Point minWithConjugateGradient(const MathFunction func, Point point,double h, int maxIter);
     static Point computeGradient(const MathFunction& func, const Point point, double h = H) {
         vector<double> grad_coords(point.coords.size());
 
@@ -65,66 +69,15 @@ public:
 
         return Point(grad_coords);
     }
-
-    static double lineSearch(const MathFunction& func, const Point& point, const Point& direction, double tol = 1e-6, double initialAlpha = INIT_ALPHA) {
-        double alpha = initialAlpha;
-        double stepSizeReduction = 0.5; // Коэффициент уменьшения шага
-        double minAlpha = tol; // Минимальный шаг, при котором остановится поиск
-        double bestAlpha = 0.0;
-        double bestValue = func.evaluate(point.coords); // Начальное значение функции
-
-        while (alpha > minAlpha) {
-            Point testPoint = point + (direction * alpha); // Новая точка в направлении direction с шагом alpha
-            double testValue = func.evaluate(testPoint.coords); // Вычисляем значение функции в новой точке
-
-            if (testValue < bestValue) {
-                bestValue = testValue;
-                bestAlpha = alpha; // Обновляем лучший найденный шаг
-            } else {
-                // Уменьшаем шаг, так как текущий не улучшает значение функции
-                alpha *= stepSizeReduction;
-            }
-        }
-
-        return bestAlpha; // Возвращаем лучший найденный шаг
+    static double findBeta(Point grad, Point gradNew, size_t iteration){
+        return (iteration % (N+1) == 0) ? (0) :
+                   (gradNew.coords[0] * gradNew.coords[0] + gradNew.coords[1] * gradNew.coords[1]) /
+               (grad.coords[0] * grad.coords[0] + grad.coords[1] * grad.coords[1]);
     }
-
-    static Point minWithConjugateGradient(const MathFunction func, Point point,double h = H, int maxIter = ITERATIONS) {
-        Point grad = computeGradient(func, point); // Градиент теперь Point
-        Point d = grad;
-        double beta, gradNorm;
-
-        swapCoords(d); // Начальное направление
-
-        for (int k = 0; k < maxIter; k++) {
-            // Поиск шага
-            double alpha = lineSearch(func, point, d);  // шаг
-            Point pointNew = point + (d * alpha);
-
-            // Вычисляем новый градиент и проверяем условие остановки
-            Point gradNew = computeGradient(func, pointNew);
-            gradNorm = sqrt(gradNew.coords[0] * gradNew.coords[0] + gradNew.coords[1] * gradNew.coords[1]);
-
-            if (gradNorm < h) {
-                return pointNew;
-            }
-
-            // Коэффициент Бета
-            beta = findBeta(grad,gradNew);
-
-            // Обновляем направление с использованием перегруженных операторов
-            d = (gradNew * -1) + (d * beta);
-            points.push_back(point);
-            // Обновляем точку и градиент
-            point = pointNew;
-            grad = gradNew;
-        }
-        return point; // Возвращаем последнюю точку
-    }
-
 
 private:
 
+    static double lineSearch(const MathFunction& func, const Point& point, const Point& direction, double tol, double initialAlpha);
     static double computePartialDerivative(const MathFunction func, const vector<double>& coords, unsigned int index, double h) {
         vector<double> pointPlusH = coords;
         vector<double> pointMinusH = coords;
@@ -134,20 +87,65 @@ private:
 
         return (func.evaluate(pointPlusH) - func.evaluate(pointMinusH)) / (2 * h);
     }
-
-    static double findBeta(Point grad, Point gradNew){
-        return (gradNew.coords[0] * gradNew.coords[0] + gradNew.coords[1] * gradNew.coords[1]) /
-               (grad.coords[0] * grad.coords[0] + grad.coords[1] * grad.coords[1]);
-    }
-
-    static void swapCoords(Point& d) {
-        for (double& v : d.coords) {
-            v = -v;
-        }
-    }
 };
 
+double conjugateGradient::lineSearch(const MathFunction& func, const Point& point, const Point& direction, double tol = 1e-6, double initialAlpha = INIT_ALPHA) {
+    double alpha = initialAlpha;
+    double stepSizeReduction = 0.5; // Коэффициент уменьшения шага
+    double minAlpha = tol; // Минимальный шаг
+    double bestAlpha = 0.0;
+    double bestValue = func.evaluate(point.coords); // Начальное знач
 
+    while (alpha > minAlpha) {
+        Point testPoint = point + (direction * alpha);
+        double testValue = func.evaluate(testPoint.coords);
+
+        if (testValue < bestValue) {
+            bestValue = testValue;
+            bestAlpha = alpha;
+        } else {
+
+            alpha *= stepSizeReduction;
+        }
+    }
+
+    return bestAlpha;
+}
+
+Point conjugateGradient::minWithConjugateGradient(const MathFunction func, Point point,double h = H, int maxIter = ITERATIONS){
+    Point grad = computeGradient(func, point); // Градиент теперь Point
+    Point d = grad * -1;
+    double beta, gradNorm;
+
+    for (size_t k = 0; k < maxIter; k++) {
+        if(k == maxIter-1){
+            cout<<"MaxIter reached" << endl;
+        }
+        // Поиск шага
+        double alpha = lineSearch(func, point, d);  // шаг
+        //double alpha = minimizeAlpha(func, point, d, 1, 1 ,1);
+        Point pointNew = point + (d * alpha);
+
+        // Вычисляем новый градиент и проверяем условие остановки
+        Point gradNew = computeGradient(func, pointNew);
+        gradNorm = sqrt(gradNew.coords[0] * gradNew.coords[0] + gradNew.coords[1] * gradNew.coords[1]);
+
+        if (gradNorm < h) {
+            return pointNew;
+        }
+
+        // Коэффициент Бета
+        beta = findBeta(grad,gradNew,k);
+
+        // Обновляем направление с использованием перегруженных операторов
+        d = (gradNew * -1) + (d * beta);
+        points.push_back(point);
+        // Обновляем точку и градиент
+        point = pointNew;
+        grad = gradNew;
+    }
+    return point; // Возвращаем последнюю точку
+}
 
 MathFunction conjugateGradient::firstMathFunc = MathFunction(
     [](const vector<double>& args) -> double {
@@ -164,7 +162,7 @@ MathFunction conjugateGradient::secondMathFunc = MathFunction(
         if (args.size() != 2) {
             throw invalid_argument("Function requires exactly 2 arguments.");
         }
-        return (-1 / ((x-3)*(x-3)*(y-4)*(y-4) + 10*(x-5)*(x-5) + (y-4)*(y-4) + 1));
+        return (-1.0 / ((x-3)*(x-3)*(y-4)*(y-4) + 10*(x-5)*(x-5) + (y-4)*(y-4) + 1));
         }
     ); // реализация 2й
 
@@ -172,23 +170,20 @@ int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
 
-    // Первое минимизирование
+
     cout << "Starting minimization of the first function..." << endl;
     points.clear();
-    Point startPoint1{-150, -100};
+    Point startPoint1{75, 126};
 
-    // Выполнение минимизации для первой функции
+
     Point minPoint1 = conjugateGradient::minWithConjugateGradient(conjugateGradient::firstMathFunc, startPoint1);
     if (!points.empty()) {
         cout << "First function minimized." << endl;
-
-        // Вывод результатов и визуализация
         for(size_t i = 0; i < points.size() - 1; ++i) {
             w.printStrelka(points[i] * MULT, points[i + 1] * MULT, Qt::blue);
         }
-
-        // Итоговый результат минимизации первой функции
         double resultFirst = conjugateGradient::firstMathFunc.evaluate(points.back().coords);
+
         cout << "Minimum of the first function at point ("
              << points.back().coords[0] << ", "
              << points.back().coords[1] << ") := "
@@ -197,21 +192,18 @@ int main(int argc, char* argv[]) {
         cout << "Error: No points found during minimization of the first function." << endl;
     }
 
-    // Второе минимизирование
+    // Вт
     cout << "\nStarting minimization of the second function..." << endl;
     points.clear();
-    Point startPoint2{0,0};
+    Point startPoint2{0,52};
 
-    // Выполнение минимизации для второй функции
     Point minPoint2 = conjugateGradient::minWithConjugateGradient(conjugateGradient::secondMathFunc, startPoint2);
     if (!points.empty()) {
         cout << "Second function minimized." << endl;
-        // Вывод результатов и визуализация
         for (size_t i = 0; i < points.size() - 1; ++i) {
             w.printStrelka(points[i] * MULT2, points[i + 1] * MULT2, Qt::red);
         }
 
-        // Итоговый результат минимизации второй функции
         double resultSecond = conjugateGradient::secondMathFunc.evaluate(points.back().coords);
         cout << "Minimum of the second function at point ("
              << points.back().coords[0] << ", "
@@ -221,7 +213,6 @@ int main(int argc, char* argv[]) {
         cout << "Error: No points found during minimization of the second function." << endl;
     }
 
-    // Показ окна
     w.show();
     return a.exec();
 }
